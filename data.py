9028da6727e0417448988561cc8972bcf108e4a7
Jeferson <mx.jeferson.12@gmail.com>
2019-10-15 00:23:58 -0300

    part I: basics

---
+++ data.py
@@ -0,0 +1,30 @@
+from time import daylight
+
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam aspernatur, neque delectus ducimus nemo consequuntur quisquam perspiciatis dicta, dolor ad laborum. Minus eligendi cumque velit distinctio ratione quam minima saepe?',
+            'author': 'Mx Jeff',
+            'create_date': '14/10/2019'
+        },
+
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam aspernatur, neque delectus ducimus nemo consequuntur quisquam perspiciatis dicta, dolor ad laborum. Minus eligendi cumque velit distinctio ratione quam minima saepe?',
+            'author': 'Mx Jeff',
+            'create_date': '14/10/2019'
+        },
+
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam aspernatur, neque delectus ducimus nemo consequuntur quisquam perspiciatis dicta, dolor ad laborum. Minus eligendi cumque velit distinctio ratione quam minima saepe?',
+            'author': 'Mx Jeff',
+            'create_date': '14/10/2019'
+        }
+    ]
+
+    return articles
