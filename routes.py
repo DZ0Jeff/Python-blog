e628b512f61ec53003f34a59696a3d1e176d3636
Jeferson <mx.jeferson.12@gmail.com>
2019-10-19 00:39:01 -0300

    chapter III: login, start

--- routes.py
+++ routes.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, flash, redirect, url_for, logging, ses
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from data import Articles
+from classes import RegisterForm
+
 
 data = Articles()
 
@@ -26,17 +28,6 @@ def router(app, database):
         return render_template('article.html', id=id)
 
 
-    class RegisterForm(Form):
-        name = StringField('Name', [validators.Length(min=1, max=50)])
-        username = StringField('Username', [validators.Length(min=4, max=25)])
-        email = StringField('Email', [validators.Length(min=6, max=50)])
-        password = PasswordField('Password', [
-            validators.DataRequired(),
-            validators.EqualTo('confirm', message='Password does not match')
-        ])
-        confirm = PasswordField('Confirm password')
-
-
     @app.route('/register', methods=['GET', 'POST'])
     def register():
         form = RegisterForm(request.form)
@@ -58,5 +49,36 @@ def router(app, database):
             flash('You are registered and can log in, Success :)')
 
             return redirect(url_for('index'))
-
+        
         return render_template('register.html', form=form)
+
+    @app.route('/login', methods=['GET','POST'])
+    def login():
+        if request.method == 'POST':
+            # get HTML fields
+            username = request.form['username']
+            password_candidate = request.form['password']
+        
+            #Cursor
+            
+            connection = database.connect()
+            cursor = connection.cursor()
+            #cursor = database.connect().cursor()
+            result = cursor.execute("SELECT * FROM users WHERE username = %s", [username])
+
+            if result > 0:
+                data = cursor.fetchone()
+                passoword = data['password']
+
+                #compare password
+                if sha256_crypt.verify(password_candidate, passoword):
+                    app.logger.info('PASSWORD MATCHED')
+                else:
+                    app.logger.info('PASSWORD DONT MATCH!')
+
+            else:
+                app.logger.info('NO USER')
+
+        return render_template('login.html')
+
+        
