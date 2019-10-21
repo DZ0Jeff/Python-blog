2f85a89573884de53246ae03cf0c8f00926a42c4
Jeferson <mx.jeferson.12@gmail.com>
2019-10-21 01:00:50 -0300

    Chapter III: login and access

--- routes.py
+++ routes.py
@@ -3,31 +3,45 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from data import Articles
 from classes import RegisterForm
+from functools import wraps
 
 
 data = Articles()
 
 def router(app, database):
+    #index
     @app.route('/')
     def index():
         return render_template('home.html')
 
-
+    #about
     @app.route('/about')
     def about():
         return render_template('about.html')
 
 
+    #list of articles
     @app.route('/articles')
     def articles():
         return render_template('articles.html', articles = data)
 
-
+    #article
     @app.route('/article/<string:id>/')
     def article(id):
         return render_template('article.html', id=id)
+    
+    # Check if user logged in
+    def is_logged_in(f):
+        @wraps(f)
+        def wrap(*args, **kwargs):
+            if 'logged_in' in session:
+                return f(*args, **kwargs)
+            else:
+                flash('Unauthorized, Please login', 'danger')
+                return redirect(url_for('login'))
+        return wrap
 
-
+    #register
     @app.route('/register', methods=['GET', 'POST'])
     def register():
         form = RegisterForm(request.form)
@@ -52,6 +66,7 @@ def router(app, database):
         
         return render_template('register.html', form=form)
 
+    #login
     @app.route('/login', methods=['GET','POST'])
     def login():
         if request.method == 'POST':
@@ -68,17 +83,40 @@ def router(app, database):
 
             if result > 0:
                 data = cursor.fetchone()
-                passoword = data['password']
+                passoword = data[4]
 
                 #compare password
                 if sha256_crypt.verify(password_candidate, passoword):
-                    app.logger.info('PASSWORD MATCHED')
+                    #passed
+                    session['logged_in'] = True
+                    session['username'] = username
+
+                    flash('You are now logged in', 'Sucess :)')
+                    return redirect(url_for('dashboard'))
+
                 else:
-                    app.logger.info('PASSWORD DONT MATCH!')
+                    #password dint match
+                    error = 'invalid login'
+                    return render_template('login.html', error=error)
+                cursor.close()
 
             else:
-                app.logger.info('NO USER')
+                # erro or null
+                error = 'Username or password not found'
+                return render_template('login.html', error=error)
 
         return render_template('login.html')
 
-        
+
+    #dashboard
+    @app.route('/dashboard')
+    @is_logged_in
+    def dashboard():
+        return render_template('dashboard.html')   
+
+    #logout
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        flash('You are now logged out')
+        return redirect(url_for('login'))
